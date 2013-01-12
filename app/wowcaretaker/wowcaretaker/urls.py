db4e6c4131accb5b8346c941dc1ae2c4c91ef5ec
Robert Veitch <Robert@Fluxflashor.net>
2013-01-11 19:46:20 -0500

    enabling django-admin

--- app/wowcaretaker/wowcaretaker/urls.py
+++ app/wowcaretaker/wowcaretaker/urls.py
@@ -8,10 +8,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wowcaretaker.views.home', name='home'),
     # url(r'^wowcaretaker/', include('wowcaretaker.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
