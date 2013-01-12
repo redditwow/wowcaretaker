995a07272a26322fe3053008c461db1e7ea27d6a
Robert Veitch <Robert@Fluxflashor.net>
2013-01-11 21:24:26 -0500

    inital post data checker. not tested

--- app/syncer/views.py
+++ app/syncer/views.py
@@ -1,1 +1,46 @@
-# Create your views here.
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, Context, loader
+from django.shortcuts import render
+
+from django.views.decorators.csrf import csrf_exempt
+
+from syncer.models import Subreddit, GithubIP
+import simplejson as json
+
+def _is_github_ip(ip):
+    allowed_ips = list(GithubIP.objects.all())
+
+    for allowed_ip in allowed_ips:
+        if ip == allowed_ip.ip_address:
+            return True
+
+    return False
+
+
+@csrf_exempt
+def subreddit(request, subreddit):
+    if request.method=="POST":
+
+        request_ip = request.META["REMOTE_ADDR"]
+        request_is_valid  = _is_github_ip(request_ip)
+
+        if request_is_valid:
+
+            try:
+                github_request = json.loads(request.POST['payload'])
+
+            except:
+                print "Request made from {ip} was valid but payload was not submitted.".format(ip=request_ip)
+                return HttpResponse("Please submit a payload.")
+
+            else:
+                print "Request made from {ip} was valid and contained a payload".format(ip=request_ip)
+                return HttpResponse("Thank you for your payload ;)")
+
+        else:
+            print "Request made from {ip} is not in our whitelist.".format(ip=request_ip)
+            return HttpResponse("You are not allowed to POST data to this area.")
+
+    else:
+        print "Request made from {ip} did not submit POST data.".format(ip=request_ip)
+        return Http404()
