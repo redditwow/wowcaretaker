5f600e4b36f1579efc9d196afe0aa58f44eb5bd4
Robert Veitch <Robert@Fluxflashor.net>
2013-01-12 23:54:09 -0500

    bash script to run our django infcgi

---
+++ app/live.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Replace these three settings.
+PROJDIR="/home/reddit/git/wowcaretaker/app"
+PIDFILE="$PROJDIR/wowcaretaker.pid"
+SOCKET="$PROJDIR/wowcaretaker.sock"
+#SETTINGS="$PROJDIR/wowcaretaker/settings_live"
+HOST="127.0.0.1"
+PORT="3995"
+
+cd $PROJDIR
+if [ -f $PIDFILE ]; then
+    kill `cat -- $PIDFILE`
+    rm -f -- $PIDFILE
+fi
+
+exec /usr/bin/env - \
+  PYTHONPATH="../python:.." \
+  ./manage.py runfcgi host=$HOST port=$PORT pidfile=$PIDFILE  method=threaded maxspare=2
+
+# --settings=wowcaretaker.settings_live
