3653662c1a290dbb03e37d9b536a8a96d8ac61b4
Robert Veitch <Robert@Fluxflashor.net>
2013-02-06 15:06:33 -0500

    fixing the administration console for syncer

--- app/syncer/admin.py
+++ app/syncer/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from syncer.models import RepoType, GithubIP, GithubRepo, Subreddit, RedditAccount
+from syncer.models import GithubIP, RedditGithubRepo, RedditGithubBranch, Subreddit, Log
 
-admin.site.register(RepoType)
 admin.site.register(GithubIP)
-admin.site.register(GithubRepo)
+admin.site.register(RedditGithubRepo)
+admin.site.register(RedditGithubBranch)
 admin.site.register(Subreddit)
-admin.site.register(RedditAccount)
+admin.site.register(Log)
