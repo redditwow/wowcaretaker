ca320d1768cd3f4bf0fff9bdc7d3d108e48ae83f
Robert Veitch <Robert@Fluxflashor.net>
2013-02-06 14:44:52 -0500

    rework of the database so we dont have to change code for simple changes

--- app/syncer/models.py
+++ app/syncer/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
-class RepoType(models.Model):
-    type = models.TextField()
-
-    def __unicode__(self):
-        return self.type
-
-
+# contains all of the ips we want to accept post data from
+# storing these in the database may increase processing
+# slightly, but it's worth it since we don't need to make
+# code changes when github decides to go crazy.
 class GithubIP(models.Model):
     ip_address = models.IPAddressField()
 
@@ -14,26 +11,33 @@ class GithubIP(models.Model):
         return self.ip_address
 
 
-class GithubRepo(models.Model):
-    name = models.TextField()
-    url = models.TextField()
-    type = models.ForeignKey(RepoType)
+# stores important information about our github repos
+class RedditGithubRepo(models.Model):
+    url = models.UrlField(primary_key=True)
+
+    # very important reddit configuration stuff.
+    # this could all be hardcoded but why limit ourselves?
+    css_folderpath = models.TextField()
+    images_folderpath = models.TextField()
+    sidebar_filepath = models.TextField()
+    description_filepath = models.TextField()
+
+    def __unicode__(self):
+        return self.url
+
+# stores our github branch data. will let us sync
+# different branches to different subreddits
+class RedditGithubBranch(models.Model):
+    name = models.TextField(primary_key=True) # retard proof.. hopefully
+    branch = models.TextField()
 
     def __unicode__(self):
         return self.name
 
 
 class Subreddit(models.Model):
-    subreddit_name = models.TextField(primary_key=True)
-    subreddit_repo = models.ForeignKey(GithubRepo)
+    name = models.TextField(primary_key=True)
+    branch = models.ForeignKey(GithubBranch)
 
     def __unicode__(self):
-        return self.subreddit_name
-
-
-class RedditAccount(models.Model):
-    username = models.TextField()
-    password = models.TextField()
-
-    def __unicode__(self):
-        return self.username
+        return self.name
