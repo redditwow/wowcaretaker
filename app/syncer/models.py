b45eb73a15360b733202dcb497bc7a8dcf4807ad
Robert Veitch <Robert@Fluxflashor.net>
2013-02-06 14:45:13 -0500

    forgot to comment my Subreddit

--- app/syncer/models.py
+++ app/syncer/models.py
@@ -34,7 +34,7 @@ class RedditGithubBranch(models.Model):
     def __unicode__(self):
         return self.name
 
-
+# all subreddits need a branch
 class Subreddit(models.Model):
     name = models.TextField(primary_key=True)
     branch = models.ForeignKey(GithubBranch)
